545490cb1a67c4af106f70685f70ca835ae28822
kevstrosky <kevinochoa.dev@gmail.com>
2023-11-28 19:34:35 -0700

    first commit

---
+++ src/app/components/simulacion.tsx
@@ -0,0 +1,132 @@
+"use client";
+import React, { useState, useEffect } from "react";
+
+interface PatientRecord {
+  id: number;
+  name: string;
+}
+
+const Simulation: React.FC = () => {
+  const [startTime, setStartTime] = useState<number | null>(null);
+  const [patients, setPatients] = useState<string[]>([]);
+  const [currentPatient, setCurrentPatient] = useState<string | null>(null);
+  const [patientCounter, setPatientCounter] = useState<number>(1);
+  const [patientRecords, setPatientRecords] = useState<PatientRecord[]>([]);
+  const [simulating, setSimulating] = useState<boolean>(false);
+  const [currentTime, setCurrentTime] = useState<number>(0);
+
+  const startSimulation = () => {
+    setSimulating(true);
+    setStartTime(Date.now());
+  };
+
+  const getTimeElapsed = () => {
+    if (!startTime) return 0;
+    return Math.floor((Date.now() - startTime) / 1000);
+  };
+
+  useEffect(() => {
+    let interval: NodeJS.Timeout;
+
+    if (simulating) {
+      interval = setInterval(() => {
+        setCurrentTime((prevTime) => prevTime + 1);
+      }, 1000);
+    }
+
+    return () => clearInterval(interval);
+  }, [simulating]);
+
+  useEffect(() => {
+    if (simulating) {
+      const interval = setInterval(() => {
+        const newPatient = `Paciente ${patientCounter}`;
+        setPatients((prevPatients) => [...prevPatients, newPatient]);
+        setPatientCounter((prevCounter) => prevCounter + 1);
+      }, 5000);
+
+      return () => clearInterval(interval);
+    }
+  }, [simulating, patientCounter]);
+
+  useEffect(() => {
+    if (simulating && patients.length > 0 && !currentPatient) {
+      const nextPatient = patients[0];
+      setCurrentPatient(nextPatient);
+      setPatients((prevPatients) => prevPatients.slice(1));
+      const consultationTime = Math.floor(Math.random() * 10) + 1;
+      setTimeout(() => {
+        setCurrentPatient(null);
+        setPatientRecords((prevRecords) => [
+          ...prevRecords,
+          { id: prevRecords.length + 1, name: nextPatient },
+        ]);
+      }, consultationTime * 1000);
+    }
+  }, [simulating, patients, currentPatient]);
+
+  return (
+    <div className="container mx-auto py-8">
+      <h1 className="text-3xl font-bold mb-4">
+        Simulación de Veterinaria (Eventos Discretos)
+      </h1>
+      {!simulating && (
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={startSimulation}
+        >
+          Iniciar Simulación
+        </button>
+      )}
+      {simulating && <p>Tiempo transcurrido: {currentTime} segundos</p>}
+
+      {simulating && (
+        <>
+          <div className="mt-8">
+            <h2 className="text-xl font-semibold">Pacientes en Espera:</h2>
+            <ul>
+              {patients.map((patient, index) => (
+                <li key={index}>{patient}</li>
+              ))}
+            </ul>
+          </div>
+
+          {currentPatient && (
+            <div className="mt-8">
+              <h2 className="text-xl font-semibold">Paciente en Consulta:</h2>
+              <p>{currentPatient}</p>
+            </div>
+          )}
+
+          {patientRecords.length > 0 && (
+            <div className="mt-8">
+              <h2 className="text-xl font-semibold">Pacientes Atendidos:</h2>
+              <table className="border-collapse border border-gray-400">
+                <thead>
+                  <tr>
+                    <th className="border border-gray-400 px-4 py-2">ID</th>
+                    <th className="border border-gray-400 px-4 py-2">Nombre</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {patientRecords.map((record) => (
+                    <tr key={record.id}>
+                      <td className="border border-gray-400 px-4 py-2">
+                        {record.id}
+                      </td>
+                      <td className="border border-gray-400 px-4 py-2">
+                        {record.name}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          )}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Simulation;
